6ecd6107e709d337fb0b3a76f752ff37c2fcd578
Speeskees <35742282+Speeskees@users.noreply.github.com>
2024-04-30 08:50:46 +0200

    Major rewrite

---
+++ CustomSoundtrack.cs
@@ -0,0 +1,368 @@
+﻿using BepInEx;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using Newtonsoft.Json;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using UnityEngine.SceneManagement;
+using Path = System.IO.Path;
+
+namespace CustomSoundtrack {
+
+    [BepInPlugin("com.speeskees.customsoundtrack", "CustomSoundtrack", "2024.5.1")]
+
+    public class CustomSoundtrack : BaseUnityPlugin {
+
+        // settings
+        private string playbackMode = "next";
+        private string trackPath;
+
+        // playlists
+        private Dictionary<string, List<string>> bossPlaylists = new Dictionary<string, List<string>>();
+        private Dictionary<string, List<string>> playlists = new Dictionary<string, List<string>>();
+
+        // current game state
+        private bool bossActive = false;
+        private string currentScene = "";
+
+        // current playback state
+        private List<string> currentPlaylist = new List<string>();
+        private string currentTrack = "";
+        private bool paused = false;
+        private bool start = false;
+
+        private AudioFileReader currentAudioFileReader;
+        private FadeInOutSampleProvider currentFadeInOutSampleProvider;
+        private WaveOutEvent player = new WaveOutEvent();
+
+        // logging
+        private bool logging = false;
+        private string logPath = "log.txt";
+
+        // thread safety
+        private bool locked = false;
+
+        // called when the game is started
+        public void Awake() {
+
+            //// read settings from "settings.json"
+
+            // load paths
+            var pluginPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            logPath = Path.Combine(pluginPath, logPath);
+            trackPath = pluginPath;
+
+            // if "log.txt" doesn't exist, create it and enable logging
+            if (!File.Exists(logPath))
+                logging = true;
+
+            // read "settings.json"
+            Settings settings = null;
+            try {
+                settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Path.Combine(pluginPath, "settings.json")));
+            } catch (Exception error) {
+                log("could not parse settings.json: " + Environment.NewLine + error);
+            }
+
+            // load settings
+            if (settings != null) {
+
+                // load track directory
+                if (settings.trackPath != null && settings.trackPath != "")
+                    trackPath = settings.trackPath;
+
+                // load playback mode
+                if (settings.playbackMode != null)
+                    playbackMode = settings.playbackMode;
+
+                // load playlists
+                if (settings.playlists != null)
+                    foreach (var playlist in settings.playlists)
+                        if (playlist.ContainsKey("scenes"))
+                            foreach (var scene in playlist["scenes"]) {
+
+                                if (!playlists.Keys.Contains(scene))
+                                    playlists.Add(scene, new List<string>());
+                                if (playlist.ContainsKey("tracks"))
+                                    foreach (var track in playlist["tracks"])
+                                        playlists[scene].Add(Path.Combine(trackPath, track));
+
+                                if (!bossPlaylists.Keys.Contains(scene))
+                                    bossPlaylists.Add(scene, new List<string>());
+                                if (playlist.ContainsKey("bossTracks"))
+                                    foreach (var bossTrack in playlist["bossTracks"])
+                                        bossPlaylists[scene].Add(Path.Combine(trackPath, bossTrack));
+
+                            }
+
+            }
+
+            // load all tracks in the track directory and subdirectories to the default playlist
+            playlists.Add("_default", new List<string>());
+            foreach (var track in Directory.GetFiles(trackPath, "*", SearchOption.AllDirectories)
+                .Where(fileName => fileName.ToLower().EndsWith(".aiff") || fileName.ToLower().EndsWith(".mp3") || fileName.ToLower().EndsWith(".wav")))
+                playlists["_default"].Add(track);
+
+            if (logging) {
+
+                log("track directory: " + trackPath);
+                log("playback mode: " + playbackMode);
+
+                log("playlists: ");
+                foreach (var scene in playlists.Keys) {
+                    log("    " + scene + ": ");
+                    foreach (var track in playlists[scene])
+                        log("        " + track);
+                }
+
+                log("boss playlists: ");
+                foreach (var scene in bossPlaylists.Keys) {
+                    log("    " + scene + ": ");
+                    foreach (var track in bossPlaylists[scene])
+                        log("        " + track);
+                }
+
+            }
+
+            //// hooks
+
+            // when a scene is loaded
+            SceneManager.sceneLoaded += (scene, mode) => {
+
+                if (scene.name == "splash")
+                    start = true;
+
+                if (start && currentScene != scene.name) {
+                    bossActive = false;
+                    currentPlaylist = playlists["_default"];
+                    currentScene = scene.name;
+                    nextPlaylist();
+                }
+
+                if (logging)
+                    log("loaded scene: " + scene.name);
+
+            };
+
+            // when you activate the teleporter
+            On.RoR2.TeleporterInteraction.OnInteractionBegin += (orig, self, activator) => {
+
+                orig(self, activator);
+
+                if (!bossActive) {
+                    bossActive = true;
+                    nextPlaylist();
+                }
+
+                if (logging)
+                    log("activated teleporter");
+
+            };
+
+            // when another player activates the teleporter
+            On.RoR2.TeleporterInteraction.RpcClientOnActivated += (orig, self, activator) => {
+
+                orig(self, activator);
+
+                if (!bossActive) {
+                    bossActive = true;
+                    nextPlaylist();
+                }
+
+                if (logging)
+                    log("activated teleporter");
+
+            };
+
+            // when the mithrix fight starts
+            On.EntityStates.Missions.BrotherEncounter.Phase1.OnEnter += (orig, self) => {
+
+                orig(self);
+
+                if (!bossActive) {
+                    bossActive = true;
+                    nextPlaylist();
+                }
+
+                if (logging)
+                    log("started mithrix fight");
+
+            };
+
+            // when the game is focussed or unfocussed
+            On.RoR2.AudioManager.AudioFocusedOnlyConVar.OnApplicationFocus += (orig, self, isFocussed) => {
+
+                orig(self, isFocussed);
+
+                if (isFocussed && player.PlaybackState == PlaybackState.Paused) {
+                    paused = false;
+                    if (!locked)
+                        player.Play();
+                }
+
+                if (!isFocussed && player.PlaybackState == PlaybackState.Playing) {
+                    paused = true;
+                    if (!locked)
+                        player.Pause();
+                }
+
+            };
+
+            // when the game tries to play music
+            On.RoR2.MusicController.PickCurrentTrack += disableOriginalSoundtrack;
+            On.RoR2.MusicTrackOverride.PickMusicTrack += disableOriginalSoundtrack;
+
+        }
+
+        private void disableOriginalSoundtrack(On.RoR2.MusicTrackOverride.orig_PickMusicTrack orig, MusicController self, ref MusicTrackDef track) {
+            orig(self, ref track);
+            track = null;
+        }
+
+        private void disableOriginalSoundtrack(On.RoR2.MusicController.orig_PickCurrentTrack orig, MusicController self, ref MusicTrackDef track) {
+            orig(self, ref track);
+            track = null;
+        }
+
+        // called on every frame
+        public void Update() {
+
+            // play the next track if playback has stopped
+            if (start && !locked && player.PlaybackState == PlaybackState.Stopped)
+                nextTrack();
+
+            applyVolume();
+
+        }
+
+        // select and start playing the next playlist
+        private void nextPlaylist() {
+
+            // try to find a playlist for the current stage
+            // if no playlist is found, the default playlist is used
+            if (bossActive == false) {
+                if (playlists.ContainsKey(currentScene) && playlists[currentScene].Count > 0)
+                    currentPlaylist = playlists[currentScene];
+            } else {
+                if (bossPlaylists.ContainsKey(currentScene) && bossPlaylists[currentScene].Count > 0)
+                    currentPlaylist = bossPlaylists[currentScene];
+                else
+                    return;
+            }
+
+            // if playbackMode is set to "next", leave the playlist unchanged and start playing the first track
+            if (playbackMode == "next") {
+                nextTrack();
+                return;
+            }
+
+            // if playbackMode is set to "shuffle", randomize the playlist and start playing the first track
+            var randomizer = new Random();
+            currentPlaylist = currentPlaylist.OrderBy(_ => randomizer.Next()).ToList();
+            if (playbackMode == "shuffle") {
+                nextTrack();
+                return;
+            }
+
+            // if playbackMode is set to "repeat", remove all but the first track from the playlist and start playing the track
+            currentPlaylist = currentPlaylist.GetRange(0, 1);
+            nextTrack();
+
+        }
+
+        // select and start the next track
+        private void nextTrack() {
+
+            if (currentPlaylist.Count > 0) {
+
+                // select the first track from the current playlist
+                var nextTrack = currentPlaylist[0];
+                if (nextTrack != currentTrack || player.PlaybackState == PlaybackState.Stopped) {
+
+                    var oldFadeInOutSampleProvider = currentFadeInOutSampleProvider;
+
+                    // update current track
+                    currentTrack = nextTrack;
+                    currentAudioFileReader = new AudioFileReader(currentTrack);
+                    currentFadeInOutSampleProvider = new FadeInOutSampleProvider(currentAudioFileReader);
+
+                    // cycle playlist
+                    currentPlaylist.Add(currentTrack);
+                    currentPlaylist.RemoveAt(0);
+
+                    applyVolume();
+
+                    if (logging) {
+
+                        log("current track: " + currentTrack);
+
+                        log("current playlist: ");
+                        foreach (var track in currentPlaylist)
+                            log("    " + track);
+
+                    }
+
+                    if (!locked) {
+
+                        locked = true;
+
+                        var newTrack = currentFadeInOutSampleProvider;
+                        var oldTrack = oldFadeInOutSampleProvider;
+
+                        new Thread(() => {
+
+                            // if still playing, fade out the current track and stop playing
+                            if (player.PlaybackState != PlaybackState.Stopped) {
+
+                                oldTrack.BeginFadeOut(1500);
+                                Thread.Sleep(1500);
+                                player.Stop();
+
+                            }
+
+                            // load the next track and start playing
+                            player.Init(newTrack);
+                            if (!paused)
+                                player.Play();
+
+                            locked = false;
+
+                        }).Start();
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        // read and apply the volume from the settings menu to the current track
+        private void applyVolume() {
+            if (currentAudioFileReader != null) {
+                float.TryParse(AudioManager.cvVolumeMaster.GetString(), out float masterVolume);
+                float.TryParse(AudioManager.cvVolumeMsx.GetString(), out float musicVolume);
+                masterVolume /= 100f;
+                musicVolume /= 100f;
+                currentAudioFileReader.Volume = masterVolume * musicVolume;
+            }
+        }
+
+        private void log(string message) {
+            File.AppendAllText(logPath, "[" + DateTime.Now.ToString("hh:mm:ss") + "] " + message + Environment.NewLine);
+        }
+
+    }
+
+    public class Settings {
+        public string playbackMode { get; set; }
+        public string trackPath { get; set; }
+        public List<Dictionary<string, List<string>>> playlists { get; set; }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,110 +1,13 @@
-# The OriginalSoundTrack Plugin
+## TODO write a better readme
 
-This mod allows you to play Risk of Rain 1 (or any) music in place of the normal soundtrack.
+An overhaul of [EtiTheSpirt's fork](https://github.com/EtiTheSpirit/RoR2-Original-Sound-Track/tree/master) of [Kyle Paulsen's OriginalSoundTrack mod](https://github.com/kylepaulsen/RoR2-Original-Sound-Track).
 
-You must supply your own music files though.
+The following improvements were made:
 
-Place the DLLs and settings.xml in:
-Risk of Rain 2\BepInEx\plugins\OriginalSoundTrack
+- the settings file has been rewritten in json
+- prevented a game freeze when switching tracks
+- added logging to help debug issues with the settings file, only enabled when there is no log yet
+- implemented getting volume from the ingame sliders
+- added all scenes since 1.5.2024 to the settings file
 
-Your music files can either go in the same directory or you can specifiy a directory
-in settings.xml (using the `<music-path>` tag).
-
-Music files can be in .mp3 or .wav format (with those exact extensions).
-
-Make sure the filenames match what's in settings.xml (or edit settings.xml to match your files.)
-
-If you delete settings.xml, don't match your filenames with `<song>` tags, or delete all `<song>` tags, then the
-plugin will just choose random music it found.
-
-## settings.xml
-
-If your files match all the song "names", then you don't need to do anything else, but feel free to customize.
-
-If the plugin fails to match a song for a scene, it will just choose a random one it found in the plugin directory.
-
-Put scene IDs in the "scenes" attribute to have audio play on that scene in settings.xml.
-The scenes attribute is comma separated. A scene only has to be "contained" in the real scene ID for it to match:
-("golemplains" in the scene attr will match for the golemplains2 scene id.)
-
-Example for playing MyFile.mp3 on the title screen and main menus:
-```
-<song
-    name="MyFile.mp3"
-    scenes="title,lobby,logbook,crystalworld,eclipseworld"
-    boss="false"
-    volume="1"
-/>
-```
-
-Setting boss to "true" means that music can only play when the player activates the teleport on a level (or on the final boss fight).
-Otherwise music can only play elsewhere (non teleport events).
-"volume" is a decimal between 0 and 1.
-
-It's ok to have multiple `<song>` definitions with the same name or scenes. The plugin will choose a song randomly
-among the matching songs.
-
-The top level `<volume>` tag is the master volume for all this plugin's music. Again, a decimal between 0 and 1.
-
-The top level `<loop>` tag determines if music should loop or pick another song (from matching songs) after a song ends.
-
-The top level `<music-path>` tag specifies where the plugin should scan for music. It does not traverse down directories.
-The default path it scans for music is: `Risk of Rain 2\BepInEx\plugins\OriginalSoundTrack`
-
-## Scene IDs (level IDs).
-
-(As of the 1.0 update)
-
-See here for pictures of the level scenes:
-https://riskofrain2.fandom.com/wiki/Environments
-
-```
-Non Levels:
-
-ID                 Description
-===============================================
-splash         -   "hopoo games" image
-intro          -   intro cutscene
-outro          -   outro cutscene
-title          -   the title screen
-lobby          -   the select character screen.
-logbook        -   view logs screen.
-crystalworld   -   prismatic trials menu (not normally reachable in modded RoR2)
-eclipseworld   -   eclipse menu
-
-Levels:
-
-ID                 Level Name / Description
-===============================================
-bazaar         -   Bazaar Between Time (The Shop)
-blackbeach     -   Distant Roost
-golemplains    -   Titanic Plains
-foggyswamp     -   Wetland Aspect
-goolake        -   Abandoned Aqueduct
-frozenwall     -   Rallypoint Delta
-wispgraveyard  -   Scorched Acres
-dampcavesimple -   Abyssal Depths
-shipgraveyard  -   Siren's Call
-rootjungle     -   Sundered Grove
-skymeadow      -   Sky Meadow
-moon           -   Commencement (Final Level)
-
-artifactworld  -   Bulwark's Ambry (Artifact Challenge arena where you unlock the artifacts with keys.)
-arena          -   Void Fields (poison area with lots of "teleport" events.)
-mysteryspace   -   A Moment, Fractured (the place where you can obliterate yourself.)
-limbo          -   A Moment, Whole (the mega scavenger boss)
-goldshores     -   Glided Coast (the place where you fight the golden Stone Titan)
-```
-
-## Other Info
-
-This plugin uses NAudio.dll https://github.com/naudio/NAudio to help load and play music at runtime.
-This is done to make it very easy for players to supply their own music. The downside is that the normal in game
-music isn't disabled (Maybe there is a way to do that, I dunno). To get around this, this plugin sets your in game
-music volume setting to 0. It tries to set it back to where it was when the game closes but I'm not sure if this works.
-Anyway you have been warned.
-
-## Licence
-
-When applicable, assume stuff is under the MIT licence ( https://opensource.org/licenses/MIT )
-I am not liable for any damages and there is no warranty etc...
+Should be pretty straightforward to use, just have a look at settings.json. For more information, visit [Kyle Paulsen's OriginalSoundTrack mod](https://github.com/kylepaulsen/RoR2-Original-Sound-Track).
